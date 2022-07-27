12c2f16168a9b1d90d2ee9f9a1f6d1d50542e88f
hardik270593 <hardikstar005@gmail.com>
2022-07-27 21:23:06 +0100

    this is my new lession 3

---
+++ homework_3.java
@@ -0,0 +1,12 @@
+package Homework;
+
+public class homework_3 {
+    public static void main(String[] args) {
+        int j = 7;
+        int i = 1;
+        System.out.println("table of 7");
+        for( i =1; i<= 10; i++){
+            System.out.println("7 * "+i +" = "+ i*j);
+        }
+    }
+}
---
+++ homework_3_1.java
@@ -0,0 +1,16 @@
+package Homework;
+
+public class homework_3_1 {
+    public static void main(String[] args) {
+        int a = 13;
+        a%=2;
+        if(a==0){
+            System.out.printf("this is even number");
+        }
+        else
+        {
+            System.out.println("this is odd number");
+        }
+
+    }
+}
